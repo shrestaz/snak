ebeaf2461f7001fca8b32213659cd906bbc17c2c
Manish Shrestha <9460504+shrestaz@users.noreply.github.com>
2022-02-13 21:25:04 +0100

    Console log for debug in prod

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,10 @@
 module.exports = {
   prefix: "",
-  purge: {
+  content: {
     enabled: true,
     content: ["./src/**/*.{html,ts}"],
   },
-  darkMode: "class", // or 'media' or 'class'
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
