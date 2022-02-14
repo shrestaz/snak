49db80386e22fba2fe2bef9de8bb9fdf9a0466d1
Manish Shrestha <9460504+shrestaz@users.noreply.github.com>
2022-02-14 08:18:28 +0100

    Revert "Fix flickering on message send 🥳"

--- snak-server/src/routes/chat-room/save-messages-for-room.ts
+++ snak-server/src/routes/chat-room/save-messages-for-room.ts
@@ -1,28 +1,18 @@
 import { Request, Response } from 'express';
-import { Socket } from 'socket.io';
-import { DefaultEventsMap } from 'socket.io/dist/typed-events';
 import { getDb } from '../../database-connection';
 import { dataCollection } from '../../enum/data-collection';
 import { Message } from '../../interfaces/message';
-import { transformDateToHumanReadable } from './helpers/transform-date-to-human-readable';
 
-export async function saveMessagesForChatRoom(
-  req: Request,
-  res: Response,
-  io: Socket<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap, any>
-) {
+export async function saveMessagesForChatRoom(req: Request, res: Response) {
   const db = await getDb();
   const { message, chatRoomId, from, sentAt } = req.body as Message;
 
   // Only save messages if it exists on the request body
   if (message) {
-    const { insertedId: _id } = await db
+    await db
       .collection<Message>(dataCollection.Messages)
       .insertOne({ message, chatRoomId, from, sentAt });
-    const enrichedMessage = transformDateToHumanReadable([
-      { _id, ...req.body },
-    ])[0];
-    io.emit('new-message', { ...enrichedMessage });
+
     res.status(201).json({ message: `Saved.` });
   }
 }
